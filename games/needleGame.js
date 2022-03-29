7b731ed825d67c9f34127d73eb7ab5c22c19dc3d
mocana1 <100725613+mocana1@users.noreply.github.com>
2022-03-29 13:50:12 -0700

    needle game update with working counter and it updates in the main menu. along with the needle image
    needle game update with working counter and it updates in the main menu. along with the needle image

--- games/needleGame.js
+++ games/needleGame.js
@@ -1,14 +1,18 @@
 //------------Variables for Game----------------------------------/
-let HighScore;
+let highScore1 = 0
 let startButton;
 let gameRun = false
 let activatePlayer = false
-
+let removeText = true
+let displayLevel = true
+ 
+//----------------------------------------------------------------/
 
 
 // happens only once when program starts
 function setupGame1(){
-    highScore1 = 0 // high score for game 1
+    img = loadImage('assets/needleEdit.png')
+
 
     gameButtonLength = 265
     gameButtonHeight = 100
@@ -19,6 +23,8 @@ function setupGame1(){
     start2y = 350;
 
 // the code below creates the start button
+    
+
     startButton = createButton('Start!')
     startButton.position(265, 200)
     startButton.size(gameButtonLength, gameButtonHeight)
@@ -32,42 +38,49 @@ function setupGame1(){
 function startGame1(){
     game1 = true
     menu = false
+    highScore = 0
+    currentLevel = 1
     clearMenu()
 
-    startButton.show(clearStartButton)
-    score1 = 0
-    
 
+    startButton.show(clearStartButton)
 
 }
 
 // happens every frame when needle game active
 function drawGame1() {
+
+
     background('red')
+    image(img, 0, 0)
     textSize(40)
     textFont()
     fill('white')
     text("Thread the Needle",200,50)
     textSize(20)
 
-    text('Score: '+score1+', High Score: '+highScore1,200,75)
+    text('Score: '+highScore+', High Score: '+highScore1,200,75)
 
+    if (displayLevel == true)
+    {
+        stroke('white')
+        text('Level ' + currentLevel, 280, 180)
+    }
 
     if (gameRun == true)
     {
         drawThread()
-        printInstruction()
         startGame()
-    }// FIX-ME: breifly displays instructions and disappears
-
+    }
+    
     if (activatePlayer == true)
     {
         playerControl()
+        if (removeText == true)
+        {
+            printInstruction()
+        }
     }
-
-    
-
-  
 
 }
 
@@ -84,7 +97,17 @@ function printInstruction ()
 {
     stroke('white')
     text('Feed the thread through the needle!', 200, 200)
-    text('Click again to control the thread!', 218, 220)
+    text('Hold to control the thread!', 218, 220)
+}
+
+function deleteText ()
+{
+    if (highScore > 1)
+    {
+        removeText = false
+    } else {
+        removeText = true
+    }
 }
 //--------------------------Player Functions------------------------//
 function playerControl()
@@ -97,6 +120,18 @@ function playerControl()
             strokeWeight(7)
             line(0, mouseY, mouseX, mouseY);
             strokeWeight(0)
+            if (mouseX > 450)
+            {
+                trackHighScore()
+                if (highScore > highScore1)
+                {
+                    highScore1 = highScore
+                }
+                activatePlayer = false
+                startButton.show(clearStartButton)
+                displayLevel = true
+            }
+            deleteText()
         }
 
     } else {
@@ -117,15 +152,20 @@ function startGame ()
         gameRun = false
     }
 }
+function trackHighScore ()
+{ 
+    ++highScore
+   ++ currentLevel
+}
 //------------------------Button Functions--------------------------//
 function clearStartButton ()
 {
     startButton.hide()
     gameRun = true
+    displayLevel = false
 }
-
 function clearGame1(){
     gameRun = false
     activatePlayer = false
+    startButton.hide()
 }// happens when back button clicked
-
